e4df864aa5c982730f488c404abecd74ea47f6fc
jojo6807 <jyj05181@gmail.com>
2023-09-14 19:13:00 +0900

    함수

--- 0914.sql
+++ 0914.sql
@@ -102,3 +102,46 @@ ORDER BY EMPNO DESC;
 SELECT EMPNO, HIREDATE, ENAME, SAL, DEPTNO FROM EMP
 ORDER BY DEPTNO ASC, HIREDATE DESC;
 
+
+
+
+-- 함수
+-- 오라클에서 함수는 내장 함수와 사용자 정의 함수로 나누어짐
+-- 내장 함수는 단일행 함수와 다중행(집계)함수로 나누어짐
+-- DUAL 테이블: 오라클의 SYS 계정에서 제공하는 테이블로 함수나 
+-- 계산식에서 테이블 참조 없이 실행해 보기 위해 제공됨(DUMMY 테이블)
+
+
+-- ABS: 절대값을 구하는 함수
+SELECT -10, ABS(-10) FROM DUAL;
+-- ROUND(숫자, 반올림할 위치): 반올림한 결과를 반환하는 함수, 
+-- 위치는 음수값도 줄 수 있음
+SELECT ROUND(1234.5678) AS ROUND, 
+    ROUND (1234.5678, 0) AS ROUND_0,
+    ROUND(1234.5678, 1) AS ROUND_1,    -- 소수점 두번째 자리
+    ROUND(1234.5678, 2) AS ROUND_2,
+    ROUND(1234.5678, -1) AS ROUND_MINUS1, --4에서 반올림
+    ROUND(1234.5678, -2) AS ROUND_MINUS2
+FROM DUAL;
+
+-- TRUNC : 버림한 결과를 반환하는 함수
+SELECT TRUNC(1234.5678) AS TRUNC, 
+    TRUNC (1234.5678, 0) AS TRUNC_0,
+    TRUNC(1234.5678, 1) AS TRUNC_1,    -- 소수점 두번째 자리
+    TRUNC(1234.5678, 2) AS TRUNC_2,
+    TRUNC(1234.5678, -1) AS TRUNC_MINUS1, --4에서 반올림
+    TRUNC(1234.5678, -2) AS TRUNC_MINUS2
+FROM DUAL;
+
+-- MOD: 나누기한 나머지 출력하는 출력
+SELECT MOD(21, 5) FROM DUAL;
+
+-- CEIL : 소수점 이하를 무조건 올림
+SELECT CEIL(12.0003) FROM DUAL;
+
+-- FLOOR : 소수점 이하를 무조건 내림
+SELECT FLOOR(12.9999999999) FROM DUAL;
+
+-- POWER : 정수 N을 정수 n만큼 제곱하는 함수
+SELECT POWER(3,4) FROM DUAL; 
+
