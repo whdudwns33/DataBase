e05ac2cad1204d1debf61ba13095c82affe69bd0
jojo6807 <jyj05181@gmail.com>
2023-09-14 11:37:55 +0900

    오름차순 내림차순

--- 0914.sql
+++ 0914.sql
@@ -1,13 +1,22 @@
-SELECT * FROM EMP;
-SELECT SAL FROM EMP;
+-- 정렬을 위한 ORDER BY 절. 
+SELECT * FROM EMP
+ORDER BY SAL ASC; --  ASC는 오름차순
 
-SELECT SAL, COMM FROM EMP; 
+--  사원 번호로 오름차순
+SELECT * FROM EMP
+ORDER BY SAL ASC;
+-- 내림차순
+SELECT * FROM EMP
+ORDER BY SAL DSC;
 
-SELECT SAL, COMM, DEPTNO 
-FROM EMP;
+-- 여러 칼럼 기준 정렬
+-- 정렬 조건을 넣지 않으면 기본적으로 내림차순
+-- 급여순으로 정렬하고 급여가 같은 경우 이름순으로 정렬
+SELECT * FROM EMP
+ORDER BY SAL, ENAME; 
+-- 정렬 키워드는 데이터의 뒤로 가야함
+-- 급여는 오름차순, 이름번호는 내림차순
+SELECT * FROM EMP
+ORDER BY SAL ASC, ENAME DESC; 
 
-SELECT SAL* 12 FROM EMP;
 
-SELECT * FROM EMP;
--- 
--- 
